19974db9f22f84f0b7b1545d08b2a0247c7c17b6
NotFredder <143998432+NotFredder@users.noreply.github.com>
2024-02-26 12:02:46 +0300

    Add files via upload

---
+++ Корж/Корж А.С/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Курсы по Chat-GPT</title>
+    <link rel="icon" href="image/kartinka.jpg">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <header>
+        <img width="100px" height="100px" src="image/kartinka.jpg" alt= "Логотип">
+        <h1>Курсы по Chat-GPT</h1>
+    </header>
+    <nav>
+        <a href="/">Главная</a>
+        <a href="/admpanel">Административная панель</a>
+    </nav>
+<main>
+    <p class="error"> </p>
+    <form>
+        <label>
+            Логин
+            <input type="text" name="login">
+        </label>
+        <label>
+            Пароль
+            <input type="password" name="password">
+        </label>
+        <button>Войти</button>
+    </form>
+</main>
+
+</body>
+</html>
